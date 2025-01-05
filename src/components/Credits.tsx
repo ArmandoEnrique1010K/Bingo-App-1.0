a42d3886e269aa3cac62b13ee573846f90390b28
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-01-04 22:05:58 -0500

    el componente credits esta listo

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -1,18 +1,76 @@
 import { useState } from "react";
 // Importa un icono de hero icons, se utiliza el siguiente formato, existe una variedad de iconos
 import { InformationCircleIcon } from '@heroicons/react/24/solid'
+import { Button, Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
 
 // Este componente es una página que muestra la información del autor que desarrollo esta aplicación
 export default function Credits() {
 
-    const [showCredits, setShowCredits] = useState(false);
+    const [isOpen, setIsOpen] = useState(true);
+
+    function open() {
+        setIsOpen(true)
+    }
+
+    function close() {
+        setIsOpen(false)
+    }
+
 
     // TODO: Convertir este componente en una ventana modal que se pueda abrir desde el menú principal
     return (
         <>
             {/* Aplicale estilos al icono para que se muestre */}
-            <button onClick={() => setShowCredits(!showCredits)}><InformationCircleIcon className='h-12 w-12 text-blue-300' aria-hidden="true" /></button>
-            {
+            <button onClick={open}><InformationCircleIcon className='h-12 w-12 text-blue-300' aria-hidden="true" /></button>
+
+            <Dialog open={isOpen} as="div" className="relative z-10 focus:outline-none " onClose={close}>
+                {/* Aplica el color de fondo con opacidad */}
+                <div className="fixed inset-0 z-10 w-screen overflow-y-auto bg-gray-800 bg-opacity-50">
+                    <div className="flex min-h-full items-center justify-center p-4">
+
+                        <DialogPanel
+                            transition
+                            className="w-full max-w-2xl rounded-lg bg-white p-8 shadow-xl transform transition-all duration-300 ease-in-out 
+                            
+                            data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+                        >
+                            <DialogTitle as="h2" className="text-4xl font-semibold text-center text-gray-900 mb-10">
+                                Créditos del autor
+                            </DialogTitle>
+                            <div className="space-y-3 text-xl text-gray-700">
+                                <p>
+                                    <span className="font-bold">Desarrollado por: ArmandoEnrique1010K</span>
+                                </p>
+                                <p>
+                                    Esta aplicación fue creada para probar todos los conocimientos adquiridos en Udemy.
+                                </p>
+                                <p>
+                                    Agradecimientos especiales a <span className="font-bold">Juan Pablo de la Torre</span>
+                                </p>
+                                <p>
+                                    <span className="font-bold">Tecnologias web utilizadas:</span> HTML, TailwindCSS, TypeScript & ReactJS
+                                </p>
+                                <p>
+                                    <span className="font-bold">Librerias utilizadas:</span> ToneJS, HeadlessUI & HeroIcons.
+                                </p>
+                                <p>
+                                    <span className="font-bold">Música:</span> Tap Out (instrumental) - The Strokes (2013)
+                                </p>
+                            </div>
+                            <div className="mt-10">
+                                <Button
+                                    onClick={close}
+                                    className="w-full py-2 px-4 bg-gray-700 text-white rounded-lg text-lg hover:bg-gray-600 focus:outline-none transition-all duration-300"
+                                >
+                                    Cerrar
+                                </Button>
+                            </div>
+                        </DialogPanel>
+                    </div>
+                </div>
+            </Dialog>
+
+            {/* {
                 showCredits === true && (
                     <>
                         <h2>Development</h2>
@@ -30,7 +88,7 @@ export default function Credits() {
 
                     </>
                 )
-            }
+            } */}
         </>
 
     )
